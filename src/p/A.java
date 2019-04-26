a0d1974e466cedca7c39e5c238c2439c933c1b5e
Yiming Tang <ytang3@gradcenter.cuny.edu>
2019-04-26 14:23:44 -0400

    if statements

--- src/p/A.java
+++ src/p/A.java
@@ -8,14 +8,21 @@ public class A {
 		Logger.getGlobal().log(Level.INFO, "hi, n0");
 	}
 	
-	public static void n1() {
+	public static void n2() {
 		Logger.getGlobal().info("hi, n1");
 	}
 	
-	public static void n2() {
-		try {
-		} catch (Exception e) {
-			Logger.getGlobal().info("hi, n2");
-		}
+	public static void n1() {
+		Logger LOGGER = Logger.getGlobal();
+		boolean isClosed = true;
+		
+		if (isClosed) 
+			LOGGER.log(Level.FINEST, "incoming {0}");
+		
+		if (LOGGER.isLoggable(Level.FINER))
+			LOGGER.log(Level.FINEST, "incoming {0}");	
+		
+		if (LOGGER.isLoggable(Level.FINE)) 
+			LOGGER.log(Level.SEVERE, "{0}");
 	}
 }
